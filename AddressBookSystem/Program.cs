44a139a4d68fec32f32b08a2a160e2bb642f81a2
bharathmighty248 <bharathmighty248@gmail.com>
2021-08-20 11:54:07 +0530

    [Bharath] Add: Creation Of Contact Implemented

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -6,7 +6,10 @@ namespace AddressBookSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Address Book System");
+            Console.WriteLine("Welcome to Address Book System");
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContact("Bharath", "Pasumarthi", "Kunkalamarru", "Karamchedu", "AP", "bharathmighty248@gmail.com", 523168, 8498017061);
+            addressBook.ViewContact();
         }
     }
 }
