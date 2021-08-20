061eff485d9befd4a6555dfe16c143d086d839a3
bharathmighty248 <bharathmighty248@gmail.com>
2021-08-20 12:05:41 +0530

    [Bharath] Add: Adding Contacts Through Console Implemented

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -8,7 +8,24 @@ namespace AddressBookSystem
         {
             Console.WriteLine("Welcome to Address Book System");
             AddressBook addressBook = new AddressBook();
-            addressBook.AddContact("Bharath", "Pasumarthi", "Kunkalamarru", "Karamchedu", "AP", "bharathmighty248@gmail.com", 523168, 8498017061);
+
+            Console.WriteLine("Enter First Name :");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name :");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address :");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter City :");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter State :");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter Email :");
+            string email = Console.ReadLine();
+            Console.WriteLine("Enter Zip :");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number :");
+            long phoneNumber = Convert.ToInt64(Console.ReadLine());
+            addressBook.AddContact(firstName, lastName, address, city, state, email, zip, phoneNumber);
             addressBook.ViewContact();
         }
     }
